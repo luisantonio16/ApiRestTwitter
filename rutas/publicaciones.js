168fe09d345cb50c524fe47a40fecd4b51ae14ae
luisantonio16 <luis16.16m@gmail.com>
2023-12-20 21:11:10 -0400

    prueba

--- rutas/publicaciones.js
+++ rutas/publicaciones.js
@@ -5,6 +5,7 @@ const auth = require("../Midelware/auth")
 const multer = require("multer")
 
 const app = express();
+
 //configuramos el multer para subir archivos
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
